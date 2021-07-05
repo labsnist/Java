3de6f2f91349bbf65049e1928d98709447fdddba
labsnist <86976069+labsnist@users.noreply.github.com>
2021-07-05 22:04:25 +0530

    Add files via upload

---
+++ PrimeUptoGivenNumber.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+class Prime
+{
+    public static void main(String args[])
+    {
+        int i, j, n, count = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number");
+        n = s.nextInt ();
+        System.out.println("The prime numbers upto" + n + "are...");
+        for (j = 2; j <= n; j++)
+        {
+            for (i = 1, count = 0; i <= j; i++)
+            {
+                if (j % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                System.out.print ("\t" + j);
+            }
+        }
+    }
+}
