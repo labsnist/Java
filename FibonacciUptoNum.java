0b25041911d1c115d8944afd52ce52f9078560f0
labsnist <86976069+labsnist@users.noreply.github.com>
2021-07-05 22:12:40 +0530

    Add files via upload

---
+++ FibonacciUptoNum.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+class Fibo
+{
+    public static void main(String args[])
+    {
+        int a=0,b=1,c=1,n;
+        Scanner s=new Scanner(System.in);
+        System.out.print("enter the number upto which fibonacci series is to be printed:");
+        n= s.nextInt();
+        System.out.print(a+"\t"+b);
+        do
+        {
+            System.out.print("\t"+c);
+            a=b;
+            b=c;
+            c=a+b;
+        }while(c<=n);
+    }
+}
---
+++ RootsQuadraticEquation.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+class Roots
+{
+    public static void main(String args[])
+    {
+        int a, b, c;
+        double d,k1, k2;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the coefficients of x^2 , x and constant");
+        a = s.nextInt();
+        b = s.nextInt();
+        c = s.nextInt();
+        d = (b * b)-(4 * a * c);
+        if(d > 0)
+        {
+            System.out.println ("roots are real and unequal");
+            k1 = (-b) + Math.sqrt (d) / (2 * a);
+            k2 = (-b) - Math.sqrt (d) / (2 * a);
+            System.out.println ("roots are " + k1 + "\t" + k2);
+        }
+        else if(d == 0)
+        {
+            System.out.println ("roots are real and equal");
+            k1 = (-b) / (2 * a);
+            System.out.println ("roots are " + k1 + "\t" + k1);
+        }
+        else
+        {
+            System.out.println ("roots are imaginary");
+        }
+    }
+}
